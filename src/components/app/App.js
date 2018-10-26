06b417ce5946033ac6d7583c981c26908e0d7215
gloriafercu <gloriafercu@gmail.com>
2018-10-26 12:24:15 +0200

    fetchPokemons to the Api with Promise.all is working

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -5,7 +5,46 @@ import { Footer } from './../footer/Footer';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pokemonsList: []
+    }
+  }
+
+  _fetchPokemonsToApi() {
+    const NUMBER_POKEMONS = 5;
+    for (let i = 1; i <= NUMBER_POKEMONS; i++) {
+      Promise.all([fetch(`https://pokeapi.co/api/v2/pokemon/${i}/`).then(response => response.json())])
+        .then(([pokemons]) => {
+          this.setState({
+            pokemonsList: [...this.state.pokemonsList, pokemons]
+          });
+        });
+    }
+  }
+
+  componentDidMount() {
+    this._fetchPokemonsToApi();
+  }
+
+  // getPokemons() {
+  //   const pokemonIds = 5;
+  //   let pokemonList = this.state.pokemons;
+  //   for (let i = 1; i <= pokemonIds; i++) {
+  //     fetch(`https://pokeapi.co/api/v2/pokemon/${i}/`)
+  //       .then(response => response.json())
+  //       .then(data => {
+  //         pokemonList.push(data);
+  //         this.setState({
+  //           pokemons: pokemonList
+  //         });
+  //       });
+  //   }
+  // }
+
   render() {
+    console.log('Lista de pokemons tras el fetch: ', this.state.pokemonsList);
     return (
       <div className="app__container">
         <Header>Soy un Header</Header>
