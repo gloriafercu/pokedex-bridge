3f5ec84c0f091a8c137f24f07f5e5497b2595a96
gloriafercu <gloriafercu@gmail.com>
2018-10-29 05:38:04 +0100

    add details styles but it is not well

--- src/components/details/Details.js
+++ src/components/details/Details.js
@@ -26,42 +26,31 @@ export class Details extends Component {
       <section className="pokemon__details__container">
         <div className="modal__background">
           <div className="modal__card">
-            <Link className="link__button--close" to='/'>
-              <button className="button--close">
-                <span className="button__text">Close</span>
-              </button>
-            </Link>
-            <div className="pokemon__details__content" >
-              <div className="cover">
-                <img className="front_default" src={poke.sprites.front_default} alt={poke.name} />
-              </div>
-              <div className="picture">
-                <img className=" back_default" src={poke.sprites.back_default} alt={poke.name} />
-              </div>
+            <div className="cover">
+              <Link className="link__button--close" to='/'>
+                <button className="button--close">
+                  <span className="button__text">Close</span>
+                </button>
+              </Link>
               <div className="picture">
                 <img className="front_shiny" src={poke.sprites.front_shiny} alt={poke.name} />
               </div>
-              <div className="picture">
-                <img className="back_shiny" src={poke.sprites.back_shiny} alt={poke.name} />
-              </div>
-              <div className="info__details">
-                <p className="pokemon__name__details">{poke.name}</p>
-                <div className="info__card">
-                  <div className="pokemon__abilities">
-                    <span className="abilities__title">Abilities: </span>
-                    {poke.abilities.map((elem, i) =>
-                      <span className="ability" key={i}>{elem.ability.name} </span>)}
-                  </div>
-                  <div className="pokemon__weight">
-                    <span className="weight__title">Weight: </span>
-                    <span className="weight__detail">{poke.weight / 10} kg</span>
-                  </div>
-                  <div className="pokemon__height">
-                    <span className="height__title">Height: </span>
-                    <span className="height__detail">
-                      {poke.height / 10} m
-										</span>
-                  </div>
+            </div>
+            <div className="info__details">
+              <p className="pokemon__name__details">{poke.name}</p>
+              <div className="info__card">
+                <div className="pokemon__abilities">
+                  <span className="abilities__title">Abilities: </span>
+                  {poke.abilities.map((elem, i) =>
+                    <span className="ability" key={i}>{elem.ability.name} </span>)}
+                </div>
+                <div className="pokemon__weight">
+                  <span className="weight__title">Weight: </span>
+                  <span className="weight__detail">{poke.weight / 10} kg</span>
+                </div>
+                <div className="pokemon__height">
+                  <span className="height__title">Height: </span>
+                  <span className="height__detail">{poke.height / 10} m</span>
                 </div>
               </div>
             </div>
