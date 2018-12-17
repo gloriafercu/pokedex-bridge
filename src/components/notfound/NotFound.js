49b65555623cc0fd7f018f744ea2b433e9169b05
gloriafercu <gloriafercu@gmail.com>
2018-12-17 23:30:45 +0100

    add 404 html page

---
+++ src/components/notfound/NotFound.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const NotFound = () => (
+  <div>
+    <h1 className="title">404!</h1>
+    <h2 className="subtitle">La página no ha sido encontrada</h2>
+  </div>
+)
